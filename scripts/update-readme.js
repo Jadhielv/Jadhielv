b5013cd61d65177b7593454d854059b20da2a9b4
Jadhiel Vélez <jadhielv@gmail.com>
2020-12-19 01:53:48 -0400

    fix: node is not defined

--- scripts/update-readme.js
+++ scripts/update-readme.js
@@ -40,14 +40,14 @@ const rootPath = path.join(__dirname, '..')
     }
   `)
 
-  const { viewer: { repositories } } = gitHubResponseForks
-  const { edges: { node } } = gitHubResponseSources
+  // const { viewer: { repositories } } = gitHubResponseForks
+  const { viewer: { repositories } } = gitHubResponseSources
   
   await execa('untoken', [
     path.join(rootPath, './README.template.md'),
     path.join(rootPath, './README.md'),
-    '--gh_repos_count', repositories.totalCount,
-    '--gh_repo_name', node.name,
-    '--gh_repo_url', node.url
+    // '--gh_repos_count', repositories.totalCount,
+    '--gh_repo_name', repositories.edges.node.name,
+    '--gh_repo_url', repositories.edges.node.url
   ], { cwd: rootPath, preferLocal: true })
 })()
